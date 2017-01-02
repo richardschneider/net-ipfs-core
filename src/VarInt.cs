8740420c0e5c276c7b6c094f097d1cefbeb1ce35
Richard Schneider <makaretu@gmail.com>
2017-01-02 22:22:48 +1300

    feat(varint): Codex for variable integer

---
+++ src/VarInt.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipfs
+{
+    /// <summary>
+    ///   A codec for a variable integer.
+    /// </summary>
+    /// <remarks>
+    ///   A <b>VarInt</b> is encoded in network byte order (Big Endian). Each byte (except the last) contains 7 bits
+    ///   of information with the most significant bit set to 1.  The last byte has MSB set to 0.
+    ///   <para>
+    ///   Negative values are not allowed.  When encountered a <see cref="NotSupportedException"/> is thrown.
+    ///   </para>
+    /// </remarks>
+    /// <seealso href="https://developers.google.com/protocol-buffers/docs/encoding#varints"/>
+    public static class Varint
+    {
+        /// <summary>
+        ///   Convert the value to its variable integer encoding.
+        /// </summary>
+        /// <param name="value">
+        ///   The value to convert.
+        /// </param>
+        /// <returns>
+        ///   A byte array representing the <paramref name="value"/> as
+        ///   a variable integer.
+        /// </returns>
+        public static byte[] Encode(long value)
+        {
+            using (var stream = new MemoryStream(64))
+            {
+                stream.WriteVarint(value);
+                return stream.ToArray();
+            }
+        }
+
+        /// <summary>
+        ///   The number of bytes required to encode the value.
+        /// </summary>
+        public static int RequiredBytes(long value)
+        {
+            return Encode(value).Length;
+        }
+
+        /// <summary>
+        ///   Convert the byte array to an <see cref="int"/>.
+        /// </summary>
+        /// <param name="bytes">
+        ///   A byte array containing the variable integer.
+        /// </param>
+        /// <param name="offset">
+        ///   Offset into <paramref name="bytes"/> to start reading from.
+        /// </param>
+        public static int DecodeInt32 (byte[] bytes, int offset = 0)
+        {
+            using (var stream = new MemoryStream(bytes, false))
+            {
+                stream.Position = offset;
+                return stream.ReadVarint32();
+            }
+        }
+
+        /// <summary>
+        ///   Convert the byte array to a <see cref="long"/>.
+        /// </summary>
+        /// <param name="bytes">
+        ///   A byte array containing the variable integer.
+        /// </param>
+        /// <param name="offset">
+        ///   Offset into <paramref name="bytes"/> to start reading from.
+        /// </param>
+        public static long DecodeInt64(byte[] bytes, int offset = 0)
+        {
+            using (var stream = new MemoryStream(bytes, false))
+            {
+                stream.Position = offset;
+                return stream.ReadVarint64();
+            }
+        }
+
+        /// <summary>
+        ///   Writes the variable integer encoding of the value to
+        ///   a stream.
+        /// </summary>
+        public static void WriteVarint(this Stream stream, long value)
+        {
+            if (value < 0)
+                throw new NotSupportedException("Negative values are not allowed for a Varint");
+
+            do
+            {
+                byte v = (byte) (value & 0x7F);
+                if (value > 0x7F)
+                    v |= 0x80;
+                stream.WriteByte(v);
+                value >>= 7;
+            } while (value != 0);
+        }
+
+        /// <summary>
+        ///   Reads a variable integer from the stream. 
+        /// </summary>
+        static int ReadVarint32(this Stream stream)
+        {
+            var value = stream.ReadVarint64();
+            if (value > int.MaxValue)
+                throw new InvalidDataException("Varint value is bigger than an Int32.MaxValue");
+            return (int)value;
+        }
+
+        /// <summary>
+        ///   Reads a variable integer from the stream. 
+        /// </summary>
+        static long ReadVarint64(this Stream stream)
+        {
+            long value = 0;
+            int shift = 0;
+            int bytesRead = 0;
+            while (true)
+            {
+                var b = stream.ReadByte();
+                if (b == -1)
+                    throw new InvalidDataException("Varint is not terminated");
+                if (++bytesRead > 9)
+                    throw new InvalidDataException("Varint value is bigger than an Int64.MaxValue");
+                value |= (long)(b & 0x7F) << shift;
+                if (b < 0x80)
+                    return value;
+                shift += 7;
+            }
+
+        }
+
+    }
+}
---
+++ test/VarintTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Ipfs
+{
+    [TestClass]
+    public class VarintTest
+    {
+        [TestMethod]
+        public void Zero()
+        {
+            var x = new byte[] { 0 };
+            Assert.AreEqual(1, Varint.RequiredBytes(0));
+            CollectionAssert.AreEqual(x, Varint.Encode(0));
+            Assert.AreEqual(0, Varint.DecodeInt32(x));
+        }
+        
+        [TestMethod]
+        public void ThreeHundred()
+        {
+            var x = new byte[] { 0xAC, 0x02 };
+            Assert.AreEqual(2, Varint.RequiredBytes(300));
+            CollectionAssert.AreEqual(x, Varint.Encode(300));
+            Assert.AreEqual(300, Varint.DecodeInt32(x));
+        }
+
+        [TestMethod]
+        public void Decode_From_Offset()
+        {
+            var x = new byte[] { 0x00, 0xAC, 0x02 };
+            Assert.AreEqual(300, Varint.DecodeInt32(x, 1));
+        }
+
+        [TestMethod]
+        public void MaxLong()
+        {
+            var x = "ffffffffffffffff7f".ToHexBuffer();
+            Assert.AreEqual(9, Varint.RequiredBytes(long.MaxValue));
+            CollectionAssert.AreEqual(x, Varint.Encode(long.MaxValue));
+            Assert.AreEqual(long.MaxValue, Varint.DecodeInt64(x));
+        }
+
+        [TestMethod]
+        public void Encode_Negative()
+        {
+            ExceptionAssert.Throws<NotSupportedException>(() => Varint.Encode(-1));
+        }
+
+        [TestMethod]
+        public void TooBig_Int32()
+        {
+            var bytes = Varint.Encode((long)Int32.MaxValue + 1);
+            ExceptionAssert.Throws<InvalidDataException>(() => Varint.DecodeInt32(bytes));
+        }
+
+        [TestMethod]
+        public void TooBig_Int64()
+        {
+            var bytes = "ffffffffffffffffff7f".ToHexBuffer();
+            ExceptionAssert.Throws<InvalidDataException>(() => Varint.DecodeInt64(bytes));
+        }
+
+        [TestMethod]
+        public void Unterminated()
+        {
+            var bytes = "ff".ToHexBuffer();
+            ExceptionAssert.Throws<InvalidDataException>(() => Varint.DecodeInt64(bytes));
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            var bytes = new byte[0];
+            ExceptionAssert.Throws<InvalidDataException>(() => Varint.DecodeInt64(bytes));
+        }
+    }
+}
