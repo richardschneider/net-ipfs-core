622b79cd58d7ef2a1423e89a01e3e226d0a77846
Richard Schneider <makaretu@gmail.com>
2017-10-27 14:18:40 +1300

    fix(docfx): get docfx working with new csproj (SDK) format

---
+++ builddocs.cmd
@@ -0,0 +1,4 @@
+REM See https://github.com/dotnet/docfx/issues/1752#issuecomment-308909959
+set MSBUILD_EXE_PATH=C:\Program Files\dotnet\sdk\2.0.0\MSBuild.dll
+copy doc\MSBuild.dll.config "C:\Program Files\dotnet\sdk\2.0.0\MSBuild.dll.config" /Y
+docfx doc\docfx.json
