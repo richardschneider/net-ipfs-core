77aed104586db5e7dbe735c8b9c481a8e222d416
Richard Schneider <makaretu@gmail.com>
2018-05-11 18:19:08 +1200

    feat(): parsing IPFS duration #59

---
+++ src/Duration.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Ipfs
+{
+    /// <summary>
+    ///   Parsing and stringifying of a <see cref="TimeSpan"/> according to IPFS.
+    /// </summary>
+    /// <remarks>
+    ///   From the <see href="https://godoc.org/time#ParseDuration">go spec</see>.
+    ///   <para>
+    ///   A duration string is a possibly signed sequence of decimal numbers, 
+    ///   each with optional fraction and a unit suffix, such as "300ms", "-1.5h" or "2h45m". 
+    ///   Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".
+    ///   </para>
+    /// </remarks>
+    public static class Duration
+    {
+        /// <summary>
+        ///   Converts the string representation of an IPFS duration
+        ///   into its <see cref="TimeSpan"/> equivalent.
+        /// </summary>
+        /// <param name="s">
+        ///   A string that contains the duration to convert.
+        /// </param>
+        /// <returns>
+        ///   A <see cref="TimeSpan"/> that is equivalent to <paramref name="s"/>.
+        /// </returns>
+        /// <exception cref="FormatException">
+        ///   <paramref name="s"/> is not a valid IPFS duration.
+        /// </exception>
+        /// <remarks>
+        ///   An empty string or "n/a" or "unknown" returns <see cref="TimeSpan.Zero"/>.
+        ///   <para>
+        ///   A duration string is a possibly signed sequence of decimal numbers, 
+        ///   each with optional fraction and a unit suffix, such as "300ms", "-1.5h" or "2h45m". 
+        ///   Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".
+        ///   </para>
+        /// </remarks>
+        public static TimeSpan Parse(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s) || s == "n/a" || s == "unknown")
+                return TimeSpan.Zero;
+
+            var result = TimeSpan.Zero;
+            using (var sr = new StringReader(s))
+            {
+                while (sr.Peek() != -1)
+                {
+                    result += ParseComponent(sr);
+                }
+            }
+
+            return result;
+        }
+
+        static TimeSpan ParseComponent(StringReader reader)
+        {
+            var value = ParseNumber(reader);
+            var unit = ParseUnit(reader);
+
+            switch (unit)
+            {
+                case "h":
+                    return TimeSpan.FromHours(value);
+                case "m":
+                    return TimeSpan.FromMinutes(value);
+                case "s":
+                    return TimeSpan.FromSeconds(value);
+                case "ms":
+                    return TimeSpan.FromMilliseconds(value);
+                case "us":
+                case "µs":
+                    return TimeSpan.FromMilliseconds(value * 0.001);
+                case "ns":
+                    return TimeSpan.FromMilliseconds(value * 0.000001);
+                case "":
+                    throw new FormatException("Missing IPFS duration unit.");
+                default:
+                    throw new FormatException($"Unknown IPFS duration unit '{unit}'.");
+            }
+        }
+
+        static double ParseNumber(StringReader reader)
+        {
+            var s = new StringBuilder();
+            while (true)
+            {
+                var c = (char)reader.Peek();
+                if (char.IsDigit(c) || c == '+' || c == '-' || c == '.')
+                {
+                    s.Append(c);
+                    reader.Read();
+                    continue;
+                }
+                return double.Parse(s.ToString(), CultureInfo.InvariantCulture);
+            }
+        }
+
+        static string ParseUnit(StringReader reader)
+        {
+            var s = new StringBuilder();
+            while (true)
+            {
+                var c = (char)reader.Peek();
+                if (char.IsDigit(c) || c == '+' || c == '-' || c == '.' || c == (char)0xFFFF)
+                    break;
+                s.Append(c);
+                reader.Read();
+            }
+
+            return s.ToString();
+        }
+    }
+}
---
+++ test/DurationTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Ipfs
+{
+
+    [TestClass]
+    public class DurationTest
+    {
+        [TestMethod]
+        public void Parsing_Examples()
+        {
+            Assert.AreEqual(TimeSpan.FromMilliseconds(300), Duration.Parse("300ms"));
+            Assert.AreEqual(TimeSpan.FromHours(-1.5), Duration.Parse("-1.5h"));
+            Assert.AreEqual(new TimeSpan(2, 45, 0), Duration.Parse("2h45m"));
+            Assert.AreEqual(new TimeSpan(0, 1, 0) + TimeSpan.FromSeconds(4.483878032), Duration.Parse("1m4.483878032s"));
+        }
+
+        [TestMethod]
+        public void Parsing_Zero()
+        {
+            Assert.AreEqual(TimeSpan.Zero, Duration.Parse("0s"));
+            Assert.AreEqual(TimeSpan.Zero, Duration.Parse("0µs"));
+            Assert.AreEqual(TimeSpan.Zero, Duration.Parse("0ns"));
+            Assert.AreEqual(TimeSpan.Zero, Duration.Parse("n/a"));
+            Assert.AreEqual(TimeSpan.Zero, Duration.Parse("unknown"));
+            Assert.AreEqual(TimeSpan.Zero, Duration.Parse(""));
+        }
+
+        [TestMethod]
+        public void Parsing_MissingNumber()
+        {
+            ExceptionAssert.Throws<FormatException>(() =>
+            {
+                var _ = Duration.Parse("s");
+            });
+        }
+
+        [TestMethod]
+        public void Parsing_MissingUnit()
+        {
+            ExceptionAssert.Throws<FormatException>(() =>
+            {
+                var _ = Duration.Parse("1");
+            }, "Missing IPFS duration unit.");
+        }
+
+        [TestMethod]
+        public void Parsing_InvalidUnit()
+        {
+            ExceptionAssert.Throws<FormatException>(() =>
+            {
+                var _ = Duration.Parse("1jiffy");
+            }, "Unknown IPFS duration unit 'jiffy'.");
+        }
+    }
+}
+
