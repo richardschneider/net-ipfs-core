10d8d9116be5ee6540d236f7d8aa775bc9a39c0d
Richard Schneider <makaretu@gmail.com>
2018-05-13 13:14:55 +1200

    fix(DagApi): use JObject as linked data

--- src/CoreApi/IDagApi.cs
+++ src/CoreApi/IDagApi.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
@@ -7,11 +8,11 @@ using System.Threading.Tasks;
 namespace Ipfs.CoreApi
 {
     /// <summary>
-    ///   Manages the IPLD Directed Acrylic Graph.
+    ///   Manages the IPLD (linked data) Directed Acrylic Graph.
     /// </summary>
     /// <remarks>
-    ///   The dag API is a replacement of the <see cref="IObjectApi"/>, which only supported 'dag-pb'.
-    ///   This API supports other IPLD formats, such as dag-cbor, ethereum-block, git, ...
+    ///   The DAG API is a replacement of the <see cref="IObjectApi"/>, which only supported 'dag-pb'.
+    ///   This API supports other IPLD formats, such as cbor, ethereum-block, git, ...
     /// </remarks>
     /// <seealso cref="IObjectApi"/>
     /// <seealso cref="ILinkedNode"/>
@@ -22,11 +23,11 @@ namespace Ipfs.CoreApi
         ///  Put an IPLD node.
         /// </summary>
         /// <param name="data">
-        ///   The data to send to the network.
+        ///   The JSON data to send to the network.
         /// </param>
         /// <param name="contentType">
-        ///   The content type or format of the <paramref name="data"/>; such as "raw" or "dag-db".
-        ///   See <see cref="MultiCodec"/> for more details.
+        ///   The content type or format of the <paramref name="data"/>; such as "raw" or "cbor".
+        ///   See <see cref="MultiCodec"/> for more details.  Defaults to "cbor".
         /// </param>
         /// <param name="multiHash">
         ///   The <see cref="MultiHash"/> algorithm name used to produce the <see cref="Cid"/>.
@@ -35,28 +36,28 @@ namespace Ipfs.CoreApi
         ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
         /// </param>
         /// <returns>
-        ///    A task that represents the asynchronous put operation. The task's value is
-        ///    the data's <see cref="Cid"/>.
+        ///   A task that represents the asynchronous put operation. The task's value is
+        ///   the data's <see cref="Cid"/>.
         /// </returns>
         Task<Cid> PutAsync(
-            ILinkedNode data,
-            string contentType,
+            JObject data,
+            string contentType = "cbor",
             string multiHash = MultiHash.DefaultAlgorithmName,
             CancellationToken cancel = default(CancellationToken));
 
         /// <summary>
         ///   Get an IPLD node.
         /// </summary>
-        /// <param name="path">
-        ///   The CID or path to an IPLD node.
+        /// <param name="id">
+        ///   The <see cref="Cid"/> of the IPLD node.
         /// </param>
         /// <param name="cancel">
         ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
         /// </param>
         /// <returns>
         ///   A task that represents the asynchronous get operation. The task's value
-        ///   contains the node's content.
+        ///   contains the node's content as JSON.
         /// </returns>
-        Task<ILinkedNode> GetAsync(string path, CancellationToken cancel = default(CancellationToken));
+        Task<JObject> GetAsync(Cid id, CancellationToken cancel = default(CancellationToken));
     }
 }
