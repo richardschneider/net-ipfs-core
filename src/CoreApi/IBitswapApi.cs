cdeaa6190b61f69adc0518cf3a7883e17dc65b6d
Richard Schneider <makaretu@gmail.com>
2018-06-05 13:48:57 +1200

    feat: define the Bitswap API #61

--- src/CoreApi/IBitswapApi.cs
+++ src/CoreApi/IBitswapApi.cs
@@ -1,17 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Ipfs.CoreApi
 {
     /// <summary>
-    ///   Interact with the bitswap agent.
+    ///   Data trading module for IPFS. Its purpose is to request blocks from and 
+    ///   send blocks to other peers in the network.
     /// </summary>
     /// <remarks>
-    ///   <note>Not yet ready for prime time.</note>
+    ///   Bitswap has two primary jobs (1) Attempt to acquire blocks from the network that 
+    ///   have been requested by the client and (2) Judiciously(though strategically)
+    ///   send blocks in its possession to other peers who want them.
     /// </remarks>
-    /// <seealso href="https://github.com/ipfs/interface-ipfs-core/blob/master/SPEC/BITSWAP.md">Bitswap API spec</seealso>
+    /// <seealso href="https://github.com/ipfs/specs/tree/master/bitswap">Bitswap spec</seealso>
     public interface IBitswapApi
     {
+        /// <summary>
+        ///   Gets a block from the IPFS network.
+        /// </summary>
+        /// <param name="id">
+        ///   The <see cref="Cid"/> of the <see cref="IDataBlock">block</see>.
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous get operation. The task's value
+        ///   contains the block's id and data.
+        /// </returns>
+        /// <remarks>
+        ///   Waits for another peer to supply the block with the <paramref name="id"/>.
+        /// </remarks>
+        Task<IDataBlock> GetAsync(Cid id, CancellationToken cancel = default(CancellationToken));
+
+        /// <summary>
+        ///   The blocks that are needed by a peer.
+        /// </summary>
+        /// <param name="peer">
+        ///   The id of an IPFS peer. If not specified (e.g. null), then the local
+        ///   peer is used.        
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation. The task's value
+        ///   contains the sequence of blocks needed by the <paramref name="peer"/>.
+        /// </returns>
+        Task<IEnumerable<Cid>> WantsAsync(MultiHash peer = null, CancellationToken cancel = default(CancellationToken));
     }
 }
