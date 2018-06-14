80ec64b40a8d5d906faf0fed058f7ff3f0165cb6
Richard Schneider <makaretu@gmail.com>
2018-06-14 17:10:26 +1200

    chore: improve test coverage

--- test/CoreApi/AddFileOptionsTest.cs
+++ test/CoreApi/AddFileOptionsTest.cs
@@ -20,5 +20,28 @@ namespace Ipfs.CoreApi
             Assert.AreEqual(false, options.Trickle);
             Assert.AreEqual(false, options.Wrap);
         }
+
+        [TestMethod]
+        public void Setting()
+        {
+            var options = new AddFileOptions
+            {
+                Pin = false,
+                ChunkSize = 2 * 1024,
+                Hash = "sha2-512",
+                OnlyHash = true,
+                RawLeaves = true,
+                Trickle = true,
+                Wrap = true
+            };
+
+            Assert.AreEqual(false, options.Pin);
+            Assert.AreEqual(2 * 1024, options.ChunkSize);
+            Assert.AreEqual("sha2-512", options.Hash);
+            Assert.AreEqual(true, options.OnlyHash);
+            Assert.AreEqual(true, options.RawLeaves);
+            Assert.AreEqual(true, options.Trickle);
+            Assert.AreEqual(true, options.Wrap);
+        }
     }
 }
