5726351709c1e06853a8a6dff62244e7d3a40ab3
Richard Schneider <makaretu@gmail.com>
2018-06-25 14:32:56 +1200

    test(NamedContent): setting of properties

---
+++ test/NamedContentTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Google.Protobuf;
+
+namespace Ipfs
+{
+    [TestClass]
+    public class NamedContentTest
+    {
+        [TestMethod]
+        public void Properties()
+        {
+            var nc = new NamedContent
+            {
+                ContentPath = "/ipfs/...",
+                NamePath = "/ipns/..."
+            };
+            Assert.AreEqual("/ipfs/...", nc.ContentPath);
+            Assert.AreEqual("/ipns/...", nc.NamePath);
+        }
+
+    }
+}
