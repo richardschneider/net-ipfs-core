b53602e05c6054533f2bd76a5b4d8384bf592167
Richard Schneider <makaretu@gmail.com>
2019-04-21 11:29:09 +1200

    feat(DH): improve DHT API #44

--- src/CoreApi/IDhtApi.cs
+++ src/CoreApi/IDhtApi.cs
@@ -12,40 +12,12 @@ namespace Ipfs.CoreApi
     /// <remarks>
     ///   The DHT is a place to store, not the value, but pointers to peers who have 
     ///   the actual value.
+    ///   <para>
+    ///   See the ongoing DHT specification at <see href="https://github.com/libp2p/specs/pull/108"/>
+    ///   </para>
     /// </remarks>
     /// <seealso href="https://github.com/ipfs/interface-ipfs-core/blob/master/SPEC/DHT.md">DHT API spec</seealso>
-    public interface IDhtApi
+    public interface IDhtApi : IPeerRouting, IContentRouting, IValueStore
     {
-        /// <summary>
-        ///   Information about an IPFS peer.
-        /// </summary>
-        /// <param name="id">
-        ///   The <see cref="MultiHash"/> ID of the IPFS peer.  
-        /// </param>
-        /// <param name="cancel">
-        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
-        /// </param>
-        /// <returns>
-        ///   A task that represents the asynchronous operation that returns
-        ///   the <see cref="Peer"/> information or a closer peer.
-        /// </returns>
-        Task<Peer> FindPeerAsync(MultiHash id, CancellationToken cancel = default(CancellationToken));
-
-        /// <summary>
-        ///   Find the providers for the specified content.
-        /// </summary>
-        /// <param name="id">
-        ///   The <see cref="Cid"/> of the content.
-        /// </param>
-        /// <param name="limit">
-        ///   The maximum number of peers to return.  Defaults to 20.
-        /// </param>
-        /// <param name="cancel">
-        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
-        /// </param>
-        /// <returns>
-        ///   A sequence of IPFS <see cref="Peer"/>.
-        /// </returns>
-        Task<IEnumerable<Peer>> FindProvidersAsync(Cid id, int limit = 20, CancellationToken cancel = default(CancellationToken));
     }
 }
---
+++ src/CoreApi/IValueStore.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ipfs.CoreApi
+{
+    /// <summary>
+    ///   A basic Put/Get interface.
+    /// </summary>
+    public interface IValueStore
+    {
+        /// <summary>
+        ///   Gets th value of a key.
+        /// </summary>
+        /// <param name="key">
+        ///   A byte array representing the name of a key.
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation that returns
+        ///   the value of the key as a byte array.
+        /// </returns>
+        Task<byte[]> GetAsync(byte[] key, CancellationToken cancel = default(CancellationToken));
+
+        /// <summary>
+        ///   Tries to get the value of a key.
+        /// </summary>
+        /// <param name="key">
+        ///   A byte array representing the name of a key.
+        /// </param>
+        /// <param name="value">
+        ///   A byte array containing the value of the <paramref name="key"/>
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation that returns
+        ///   <b>true</b> if the key exists; otherwise, <b>false</b>.
+        /// </returns>
+        Task<bool> TryGetAsync(byte[] key, out byte[] value, CancellationToken cancel = default(CancellationToken));
+
+        /// <summary>
+        ///   Put the value of a key.
+        /// </summary>
+        /// <param name="key">
+        ///   A byte array representing the name of a key.
+        /// </param>
+        /// <param name="value">
+        ///   A byte array containing the value of the <paramref name="key"/>
+        /// </param>
+        /// <param name="cancel">
+        ///   Is used to stop the task.  When cancelled, the <see cref="TaskCanceledException"/> is raised.
+        /// </param>
+        /// <returns>
+        ///   A task that represents the asynchronous operation.
+        /// </returns>
+        Task PutAsync(byte[] key, out byte[] value, CancellationToken cancel = default(CancellationToken));
+    }
+}
