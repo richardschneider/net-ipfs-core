9d1e09c5530b0d7839e5b9c01f4b131ae54c97de
Richard Schneider <makaretu@gmail.com>
2019-06-15 18:56:02 +1200

    feat(IBitswapApi): ledger with another peer

---
+++ src/CoreApi/BitswapLedger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ipfs.CoreApi
+{
+    /// <summary>
+    ///   Statistics on the <see cref="IBitswapApi">bitswap</see> blocks exchanged with another <see cref="Peer"/>.
+    /// </summary>
+    /// <seealso cref="IBitswapApi.LedgerAsync"/>
+    public class BitswapLedger
+    {
+        /// <summary>
+        ///   The <see cref="Peer"/> that pertains to this ledger.
+        /// </summary>
+        /// <value>
+        ///   The peer that is being monitored.
+        /// </value>
+        public Peer Peer;
+
+        /// <summary>
+        ///   The number of blocks sent by the <see cref="Peer"/> to us.
+        /// </summary>
+        /// <value>
+        ///   The number of blocks.
+        /// </value>
+        public ulong BlocksReceived;
+
+        /// <summary>
+        ///   The number of blocks sent by us to the <see cref="Peer"/>.
+        /// </summary>
+        /// <value>
+        ///   The number of blocks.
+        /// </value>
+        public ulong BlocksSent;
+
+        /// <summary>
+        ///   The number of bytes sent by the <see cref="Peer"/> to us.
+        /// </summary>
+        /// <value>
+        ///   The number of bytes.
+        /// </value>
+        public ulong DataReceived;
+
+
+        /// <summary>
+        ///   The number of bytes sent by us to the <see cref="Peer"/>
+        /// </summary>
+        /// <value>
+        ///   The number of bytes.
+        /// </value>
+        public ulong DataSent;
+
+    }
+}
---
+++ test/CoreApi/BitswapLedgerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ipfs.CoreApi
+{
+    [TestClass]
+    public class BitswapLedgerTest
+    {
+        [TestMethod]
+        public void Defaults()
+        {
+            var ledger = new BitswapLedger();
+
+            Assert.IsNull(ledger.Peer);
+            Assert.AreEqual(0ul, ledger.BlocksReceived);
+            Assert.AreEqual(0ul, ledger.BlocksSent);
+            Assert.AreEqual(0ul, ledger.DataReceived);
+            Assert.AreEqual(0ul, ledger.DataSent);
+        }
+
+
+    }
+}
