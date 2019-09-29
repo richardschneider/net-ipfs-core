3b7497fc7cd072f10a0bb0e5ae0eca353229b7d4
Richard Schneider <makaretu@gmail.com>
2019-09-30 11:11:02 +1300

    test: more code coverage

---
+++ test/CoreApi/PingResultTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ipfs.CoreApi
+{
+    [TestClass]
+    public class PingResultTest
+    {
+        [TestMethod]
+        public void Properties()
+        {
+            var time = TimeSpan.FromSeconds(3);
+            var r = new PingResult
+            {
+                Success = true,
+                Text = "ping",
+                Time = time
+            };
+            Assert.AreEqual(true, r.Success);
+            Assert.AreEqual("ping", r.Text);
+            Assert.AreEqual(time, r.Time);
+        }
+
+    }
+}
